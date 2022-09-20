dc38f1a6deea47662e9065c065390df9941a9b7b
diego <diego@PC>
2022-09-19 22:36:03 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 1;
+            b = 1;
+            if (a == b)
+            {
+                Console.WriteLine("son iguales");
+                Console.ReadKey();
+            }
+        }
+    }
+}
