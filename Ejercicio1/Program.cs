c4c0d806d90b3832705dbf9a9968214a013a1be1
Diegojoa2015 <Diegojoa2015@gmail.com>
2022-09-19 23:31:51 -0300

    cambios

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,13 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            int a;
+            float b, c;
+            a = 10;
+            b = (float)30.5;
+            c = (float)(a + b);
+            Console.WriteLine("{0}+{1}={2}", a, b, c);
+            Console.ReadKey();
         }
     }
 }
--- Ejercicio6/Program.cs
+++ Ejercicio6/Program.cs
@@ -10,6 +10,31 @@ namespace Ejercicio6
     {
         static void Main(string[] args)
         {
+            string cadena;
+            int leg, sdo, tot, cont;
+            float prom;
+            tot = cont = 0;
+            prom = 0;
+            Console.WriteLine("ingrese el legajo");
+            cadena = Console.ReadLine();
+            leg = Convert.ToInt32(cadena);
+            while (leg != 0)
+            {
+                Console.WriteLine("igrese el sueldo");
+                cadena = Console.ReadLine();
+                sdo = Convert.ToInt32(cadena);
+                tot = tot + sdo;
+                cont = cont + 1;
+                Console.WriteLine("ingrese el legajo");
+                cadena = Console.ReadLine();
+                leg = Convert.ToInt32(cadena);
+            }
+            Console.WriteLine("el total pagado es {0}\n", tot);
+            Console.ReadKey();
+            prom = (float)(tot / cont);
+            Console.WriteLine("el sueldo promedio es {0,2:F2}", prom);
+            Console.WriteLine("el sueldo promedio es {0}", prom);
+            Console.ReadKey();
         }
     }
 }
